6f69a7b7029cd177327a1724a174be0c5f9adae2
Chibchakum <CaptainTrips1500@proton.me>
2024-07-24 20:59:43 -0400

    Create Pokemon object

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,7 +1,39 @@
-//Declare all of the choices for the three variables
+/*Declare all of the choices for the three variables
 const pokemon = ['Pikachu', 'Bulbasaur', 'Squirtle', 'Charmander', 'Eevee'];
 const attack = ['thunder shock', 'vine whip', 'water gun', 'ember', 'takedown'];
 const damage = ['10 HP of damage, not very effective', '20 HP of damage', '40 HP of damage, super effective!'];
+*/
+
+//Create objects for the pokemon
+let choosePokemon = {
+    team: {
+       choice1: {
+        pokemon: 'Pikachu',
+        attack: 'thunder shock',
+        type: 'electric',
+       },
+       choice2: {
+        pokemon: 'Bulbasaur',
+        attack: 'vine whip',
+        type: 'grass',
+       },
+       choice3: {
+        pokemon: 'Squirtle',
+        attack: 'water gun',
+        type: 'water',
+       },
+       choice4: {
+        pokemon: 'Charmander',
+        attack: 'ember',
+        type: 'fire',
+       },
+       choice5: {
+        pokemon: 'Sandshrew',
+        attack: 'dig',
+        type: 'ground',
+       },
+    }
+};
 
 //Declare the statement
 let message = `${pokemon}! I choose you! Use ${attack}! It did ${damage}`
