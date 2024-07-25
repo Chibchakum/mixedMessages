1073e1d63de24fb4179e6f38fc98a14179dc976b
Chibchakum <CaptainTrips1500@proton.me>
2024-07-24 20:36:22 -0400

    Add variables and message

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,8 @@
-console.log('Test')
+//Declare all of the choices for the three variables
+const pokemon = ['Pikachu', 'Bulbasaur', 'Squirtle', 'Charmander', 'Eevee'];
+const attack = ['thunder shock', 'vine whip', 'water gun', 'ember', 'takedown'];
+const damage = ['10 HP of damage, not very effective', '20 HP of damage', '40 HP of damage, super effective!'];
+
+//Declare the statement
+let message = `${pokemon}! I choose you! Use ${attack}! It did ${damage}`
+
